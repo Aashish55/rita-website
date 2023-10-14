c837af28450fc6aa83ac21bb7099b2f692ead674
Aashish Jangam <aashish.jangam55@gmail.com>
2023-10-14 23:02:57 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import BlogDetails from "./pages/BlogDetails"
+
+function App() {
+
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/blog/:id' element={<BlogDetails />} />
+          {/* <Route path="/*" element={<NotFound />} /> */}
+        </Routes>
+      </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/BlogDetails.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import Layout from "../layout/layout";
+import blogData from "../data/blogs.json"
+import { useLocation, useNavigate } from "react-router-dom";
+
+const BlogDetails = () => {
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  const [blog, setBlog] = useState(null)
+
+  const convertToSlug = Text => {
+    return Text.toLowerCase()
+      .replace(/ /g, "-")
+      .replace(/[^\w-]+/g, "")
+  }
+
+  useEffect(() => { 
+    let found = false
+    blogData.forEach(blog => { 
+      if (convertToSlug(blog.name) === location.pathname.split('/blog/')[1]) { 
+        setBlog(blog)
+        found = true
+      }
+    })
+    if (!found) { 
+      navigate('/')
+    }
+  }, [])
+
+  console.log(blog)
+
+  return <Layout>
+    <div className="blogDetails">
+      <div
+        style={{ backgroundImage: `url(${blog?.image})` }}
+        className="blogCoverImage"
+      />
+      <div className="textContent">
+        <div className="title">
+          {blog?.name}
+        </div>
+        <div
+          className="blogBody"
+          dangerouslySetInnerHTML={{ __html: blog?.description }}
+        />
+      </div>
+    </div>
+  </Layout>;
+};
+
+export default BlogDetails;
